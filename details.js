1fd9d7bf50fa6234772f9b25e0732d7afced8965
musfikur-rahman <musfik.rizvi@gmail.com>
2022-08-31 19:30:39 +0600

    Update details.js

--- details.js
+++ details.js
@@ -8,62 +8,50 @@ document.onreadystatechange = function () {
 }
 
 async function RestCountriesByFetch() {
-    let response = await fetch('https://restcountries.com/v2/all');
+    let response = await fetch('https://restcountries.com/v3.1/all');
     try {
         let data = await response.text();
         let obj = JSON.parse(data);
         var textSet = '';
         textSet += '<tr>';
         textSet += '<th><b>Name</b></th>';
-        textSet += '<th><b>Top Level Domain</b></th>';
-        textSet += '<th><b>Alpha 2 Code</b></th>';
-        textSet += '<th><b>Calling Codes</b></th>';
-        textSet += '<th><b>Capital</b></th>';
-        textSet += '<th><b>Alt Spellings</b></th>';
-        textSet += '<th><b>Region</b></th>';
-        textSet += '<th><b>Subregion</b></th>';
-        textSet += '<th><b>Population</b></th>';
-        textSet += '<th><b>Latlng</b></th>';
-        textSet += '<th><b>Demonym</b></th>';
-        textSet += '<th><b>Area</b></th>';
-        textSet += '<th><b>Gini</b></th>';
-        textSet += '<th><b>Timezones</b></th>';
-        textSet += '<th><b>Borders</b></th>';
-        textSet += '<th><b>Native Name</b></th>';
-        textSet += '<th><b>Numeric Code</b></th>';
-        textSet += '<th><b>Currencies</b></th>';
-        textSet += '<th><b>Languages</b></th>';
-        textSet += '<th><b>Translations</b></th>';
-        textSet += '<th><b>Regional Blocs</b></th>';
-        textSet += '<th><b>Cioc</b></th>';
+        textSet += '<th><b>official</b></th>';
+        textSet += '<th><b>nativeName</b></th>';
+        textSet += '<th><b>tld</b></th>';
+        textSet += '<th><b>cca2</b></th>';
+        textSet += '<th><b>ccn3</b></th>';
+        
+
+
+        
         textSet += '<th><b>Flag</b></th>';
         textSet += '</tr>';
         for (i = 0; i < obj.length; i++) {
             textSet += '<tr>';
-            textSet += '<td>' + obj[i].name + '</td>';
-            textSet += '<td>' + obj[i].topLevelDomain + '</td>';
-            textSet += '<td>' + obj[i].alpha2Code + '</td>';
-            textSet += '<td>' + obj[i].alpha3Code + '</td>';
-            textSet += '<td>' + obj[i].callingCodes + '</td>';
-            textSet += '<td>' + obj[i].capital + '</td>';
-            textSet += '<td>' + obj[i].altSpellings + '</td>';
-            textSet += '<td>' + obj[i].region + '</td>';
-            textSet += '<td>' + obj[i].subregion + '</td>';
-            textSet += '<td>' + obj[i].population + '</td>';
-            textSet += '<td>' + obj[i].latlng + '</td>';
-            textSet += '<td>' + obj[i].demonym + '</td>';
-            textSet += '<td>' + obj[i].area + '</td>';
-            textSet += '<td>' + obj[i].gini + '</td>';
-            textSet += '<td>' + obj[i].timezones + '</td>';
-            textSet += '<td>' + obj[i].borders + '</td>';
-            textSet += '<td>' + obj[i].nativeName + '</td>';
-            textSet += '<td>' + obj[i].numericCode + '</td>';
-            textSet += '<td>' + obj[i].currencies + '</td>';
-            textSet += '<td>' + obj[i].languages + '</td>';
-            textSet += '<td>' + obj[i].translations + '</td>';
-            textSet += '<td>' + obj[i].regionalBlocs + '</td>';
-            textSet += '<td>' + obj[i].cioc + '</td>';
-            textSet += '<td><img src=' + obj[i].flag + ' class="w3-image" ></td>';
+            textSet += '<td>' + obj[i].name.common + '</td>';
+            textSet += '<td>' + obj[i].name.official + '</td>';
+            let nativeNameValue = '';
+            for (let x in obj[i].name.nativeName) { 
+                nativeNameValue += '[' + x + '- '; 
+                for (let y in obj[i].name.nativeName[x]) { 
+                    nativeNameValue += y + ': ' + obj[i].name.nativeName[x][y] + ', '
+                } 
+                nativeNameValue += '] '
+            } 
+        textSet += '<td>' + nativeNameValue + '</td>';
+        textSet += '<td>' + obj[i].tld + '</td>';
+        textSet += '<td>' + obj[i].cca2 + '</td>';
+        textSet += '<td>' + obj[i].ccn3 + '</td>';
+
+        
+
+            
+
+
+
+
+
+            textSet += '<td><img src=' + obj[i].flags.png + ' class="w3-image" ></td>';
             textSet += '</tr>';
         }
         document.getElementById('tableData').innerHTML = textSet;
